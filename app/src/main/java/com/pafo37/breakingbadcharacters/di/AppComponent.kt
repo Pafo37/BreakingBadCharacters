2f94e41c8038b1fde54245278d67786e3c6f939a
Pafo37 <pafo96@mail.bg>
2020-08-20 11:06:04 +0300

    Set up Dagger Set up navigation graph Add libraries

---
+++ app/src/main/java/com/pafo37/breakingbadcharacters/di/AppComponent.kt
@@ -0,0 +1,23 @@
+package com.pafo37.breakingbadcharacters.di
+
+import android.app.Application
+import com.pafo37.breakingbadcharacters.App
+import com.pafo37.breakingbadcharacters.di.module.ViewModelModule
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjectionModule
+import dagger.android.AndroidInjector
+import javax.inject.Singleton
+
+@Singleton
+@Component(
+    modules = [
+        AndroidInjectionModule::class,
+        ViewModelModule::class]
+)
+interface AppComponent : AndroidInjector<App> {
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance application: Application): AppComponent
+    }
+}
---
+++ app/src/main/java/com/pafo37/breakingbadcharacters/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pafo37.breakingbadcharacters.ui
+
+import android.os.Bundle
+import com.pafo37.breakingbadcharacters.R
+import dagger.android.support.DaggerAppCompatActivity
+
+class MainActivity : DaggerAppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
